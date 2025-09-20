e858173773ceedb747ffb7fb16f78dc208f609a9
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-09-20 18:54:53 +0700

    ccmean (ccostr) With (In) R Software

---
+++ ccmean/ccmean.R
@@ -0,0 +1,20 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Calculates estimates of mean cost given censored data Use ccmean (ccostr) With (In) R Software
+# Estimates of mean cost with censored data Use ccmean (ccostr) With (In) R Software
+install.packages("remotes")
+remotes::install_github("HaemAalborg/ccostr")
+library("ccostr")
+ccmean = read.csv("https://raw.githubusercontent.com/timbulwidodostp/ccmean/main/ccmean/ccmean.csv",sep = ";")
+# Estimation
+# Calculates estimates of mean cost given censored data Use ccmean (ccostr) With (In) R Software
+# Estimates of mean cost with censored data Use ccmean (ccostr) With (In) R Software
+ccmean <- ccmean(ccmean, L = 1000)
+ccmean
+# Calculates estimates of mean cost given censored data Use ccmean (ccostr) With (In) R Software
+# Estimates of mean cost with censored data Use ccmean (ccostr) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
